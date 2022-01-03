e93d57d9412be48bbfcbdf95a259b4f0a07212d2
Ronak <ro@Ronaks-MacBook-Pro.local>
2022-01-03 16:38:41 -0600

    Update scaffolding

--- digi-deal-server/server.js
+++ digi-deal-server/server.js
@@ -5,19 +5,35 @@ import { Server } from "socket.io";
 const port = process.env.PORT || 4000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
-
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let interval;
 
 io.on("connection", (socket) => {
   console.log("New client connected" + socket.id);
-  console.log(socket);
+
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
 
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
 server.listen(port, () => {
-  console.log("Listening on :3000");
+  console.log(`Listening on :${port}`);
 });
